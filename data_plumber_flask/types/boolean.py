6f9f19241694d890724aabb8ddfcbea31ff03d7f
RichtersFinger <steffen.finger.privat@gmail.com>
2024-04-08 22:31:31 +0200

    [boolean] implement Boolean-_DPType

--- data_plumber_flask/types/boolean.py
+++ data_plumber_flask/types/boolean.py
@@ -1,4 +1,14 @@
-from . import _DPType
+from typing import Any
+
+from . import _DPType, Responses
+
 
 class Boolean(_DPType):
-    pass
+    TYPE = bool
+
+    def make(self, json, loc: str) -> tuple[Any, str, int]:
+        return (
+            self.TYPE(json),
+            Responses.GOOD.msg,
+            Responses.GOOD.status
+        )
