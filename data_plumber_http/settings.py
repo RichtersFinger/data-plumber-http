8576349e453102edfbcd555b3b6949f993940d70
RichtersFinger <steffen.finger.privat@gmail.com>
2024-05-11 13:32:57 +0200

    [settings] move pre-defined response definition to class from constructor
    (better IDE integration with tooltips)

--- data_plumber_http/settings.py
+++ data_plumber_http/settings.py
@@ -17,88 +17,61 @@ class Responses:
 
     # pylint: disable=no-member
     _instance = None
-    # define placeholders to prevent pylint's no-member-errors
     warn_on_change = False
-    GOOD = ProblemInfo("", 0)
-    MISSING_OPTIONAL = ProblemInfo("", 0)
-    UNKNOWN_PROPERTY = ProblemInfo("", 0)
-    MISSING_REQUIRED = ProblemInfo("", 0)
-    BAD_TYPE = ProblemInfo("", 0)
-    BAD_VALUE = ProblemInfo("", 0)
-    RESOURCE_NOT_FOUND = ProblemInfo("", 0)
-    CONFLICT = ProblemInfo("", 0)
-    MISSING_REQUIRED_ONEOF = ProblemInfo("", 0)
-    BAD_VALUE_IN_ONEOF = ProblemInfo("", 0)
-    MULTIPLE_ONEOF = ProblemInfo("", 0)
-    MISSING_REQUIRED_ALLOF = ProblemInfo("", 0)
-    BAD_VALUE_IN_ALLOF = ProblemInfo("", 0)
+    GOOD = ProblemInfo(
+        "OK", 0
+    )
+    MISSING_OPTIONAL = ProblemInfo(
+        "Missing optional arg.", 1
+    )
+    UNKNOWN_PROPERTY = ProblemInfo(
+        "Argument '{origin}' in '{loc}' not allowed ({accepted}).",
+        400
+    )
+    MISSING_REQUIRED = ProblemInfo(
+        "Object '{loc}' missing required property '{origin}'.",
+        400
+    )
+    BAD_TYPE = ProblemInfo(
+        "Argument '{origin}' in '{loc}' has bad type. Expected '{xp_type}' but found '{fnd_type}'.",
+        422
+    )
+    BAD_VALUE = ProblemInfo(
+        "Value '{origin}' in '{loc}' not allowed (expected {expected}).",
+        422
+    )
+    RESOURCE_NOT_FOUND = ProblemInfo(
+        "Could not find requested resource '{res}' given in '{loc}'.",
+        404
+    )
+    CONFLICT = ProblemInfo(
+        "Resource '{res}' given in '{loc}' conflicts with existing resource.",
+        409
+    )
+    MISSING_REQUIRED_ONEOF = ProblemInfo(
+        "Expected at least one match for one of {options} in '{loc}' ({details}).",
+        400
+    )
+    BAD_VALUE_IN_ONEOF = ProblemInfo(
+        "{child}",  # filled with child's message
+        2  # gets overridden by child's status
+    )
+    MULTIPLE_ONEOF = ProblemInfo(
+        "Expected exclusive match among {property} {options} in '{loc}' (got matches {matches}).",
+        400
+    )
+    MISSING_REQUIRED_ALLOF = ProblemInfo(
+        "Missing or invalid required {property} {missing} in '{loc}' ({details}).",
+        400
+    )
+    BAD_VALUE_IN_ALLOF = ProblemInfo(
+        "{child}",  # filled with child's message
+        2  # gets overridden by child's status
+    )
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.warn_on_change = False
-            cls._instance.update(
-                "GOOD", "OK", 0
-            )
-            cls._instance.update(
-                "MISSING_OPTIONAL", "Missing optional arg.", 1
-            )
-            cls._instance.update(
-                "UNKNOWN_PROPERTY",
-                "Argument '{origin}' in '{loc}' not allowed ({accepted}).",
-                400
-            )
-            cls._instance.update(
-                "MISSING_REQUIRED",
-                "Object '{loc}' missing required property '{origin}'.",
-                400
-            )
-            cls._instance.update(
-                "BAD_TYPE",
-                "Argument '{origin}' in '{loc}' has bad type. Expected '{xp_type}' but found '{fnd_type}'.",
-                422
-            )
-            cls._instance.update(
-                "BAD_VALUE",
-                "Value '{origin}' in '{loc}' not allowed (expected {expected}).",
-                422
-            )
-            cls._instance.update(
-                "RESOURCE_NOT_FOUND",
-                "Could not find requested resource '{res}' given in '{loc}'.",
-                404
-            )
-            cls._instance.update(
-                "CONFLICT",
-                "Resource '{res}' given in '{loc}' conflicts with existing resource.",
-                409
-            )
-            cls._instance.update(
-                "MISSING_REQUIRED_ONEOF",
-                "Expected at least one match for one of {options} in '{loc}' ({details}).",
-                400
-            )
-            cls._instance.update(
-                "BAD_VALUE_IN_ONEOF",
-                "{child}",  # filled with child's message
-                2  # gets overridden by child's status
-            )
-            cls._instance.update(
-                "MULTIPLE_ONEOF",
-                "Expected exclusive match among {property} {options} in '{loc}' (got matches {matches}).",
-                400
-            )
-            cls._instance.update(
-                "MISSING_REQUIRED_ALLOF",
-                "Missing or invalid required {property} {missing} in '{loc}' ({details}).",
-                400
-            )
-            cls._instance.update(
-                "BAD_VALUE_IN_ALLOF",
-                "{child}",  # filled with child's message
-                2  # gets overridden by child's status
-            )
-            cls._instance.warn_on_change = False
             cls._instance.INTERNAL_RESPONSES = list(
                 k for k, v in cls.__dict__.items()
                 if isinstance(v, ProblemInfo)
